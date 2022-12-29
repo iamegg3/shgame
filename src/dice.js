cc7587b85ab71c64175a42b2895b33159eb0ffa0
Nesaan <you@example.com>
2022-12-29 15:37:12 -0500

    Added stub dice class

---
+++ src/dice.js
@@ -0,0 +1,29 @@
+/**
+ * Rolls a regular six-side dice (Return an integer from 1-6)
+ */
+function rollDice(){
+  //to be implemented
+}
+
+
+/**
+ * Rolls a 3-sided dice that (Return an integer from 1-3)
+ */
+function rollSmallDice(){
+  //to be implemented
+}
+
+
+/**
+ * Rolls a regular dice and a small dice and return sum
+ */
+function movementRoll(){
+  //to be implemented
+}
+
+/**
+ * Rolls a regular dice and a small dice and returns difference
+ */
+function attackRoll(){
+  //to be implemented
+}
